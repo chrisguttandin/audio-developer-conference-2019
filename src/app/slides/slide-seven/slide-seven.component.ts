0f78f87abe2c545e01fb0244e7fef7b160863790
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-07-28 23:35:31 +0200

    refactor: remove obsolete standalone config

--- src/app/slides/slide-seven/slide-seven.component.ts
+++ src/app/slides/slide-seven/slide-seven.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'adc-slide-seven',
-    standalone: true,
     styleUrls: ['./slide-seven.component.scss'],
     templateUrl: './slide-seven.component.html'
 })
--- src/app/slides/slide-twenty-six/slide-twenty-six.component.ts
+++ src/app/slides/slide-twenty-six/slide-twenty-six.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
     selector: 'adc-slide-twenty-six',
-    standalone: true,
     templateUrl: './slide-twenty-six.component.html'
 })
 export class SlideTwentySixComponent {}
